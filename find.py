cfe316d7727da4f9cf0e6cfeb2e72a0dc8869956
Landon Miller <millerlm012@gmail.com>
2023-04-02 14:58:00 -0500

    Init

---
+++ find.py
@@ -0,0 +1,17 @@
+"""
+The purpose of this program is to find all apartments in a specified radius of a specific climbing gym.
+
+NEEDED API's:
+- Google Maps (Distance Matrix) | https://developers.google.com/maps/documentation/distance-matrix/start
+- Aparments.com | https://api.apartments.com/v1/
+"""
+
+import requests
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
