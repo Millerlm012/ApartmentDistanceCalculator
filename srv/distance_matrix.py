35d9aa3f4d9d4ae3fe5dc457a85a8b83d33d90aa
Conner <cook.r.conner@gmail.com>
2023-04-02 17:22:47 -0500

    Moved the file into the srv Started working on adding the distance to the apartments

---
+++ srv/distance_matrix.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def distance_matrix(apartments, gym_address, key):
+    origins = ""
+    for apart in apartments:
+        origins += apart.get("address") + "|"
+    origns = origns[:-1]
+    
+    url = f"https://maps.googleapis.com/maps/api/distancematrix/json?origins={origins}&destinations={gym_address}&key={key}"
+    payload={}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.text
+
+def add_distance_to_apartments(apartments, distance_response):
+    matrix = json.loads(distance_response)
+    rows = matrix["rows"]
+    index = 0
+    for entry in aparments:
+        entry["dist"] = rows[0].get("elements")
+
+
