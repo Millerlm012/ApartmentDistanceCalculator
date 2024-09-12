c07bb232127f3a022caffa7b93a09fb79b4b459b
Landon Miller <millerlm012@gmail.com>
2024-09-12 15:12:21 -0500

    Unsure of what these updates are
    - pushing to backup and clear my laptop

--- srv/apartments.py
+++ srv/apartments.py
@@ -1,41 +1,48 @@
 """
-apartments.py is used for querying https://apartments.com for apartments in a specfied city, state. 
+apartments.py is used for querying https://apartments.com for apartments in a specfied city, state.
 We're then able to parse the results and calculate the distance from a specified climbing gym.
 """
 
 import requests
 from bs4 import BeautifulSoup
 
-def query_apartments(city, state, page=1):
-    assert len(state) == 2, 'Please enter state abbreviation instead of the full state.'
 
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0'}
+def query_apartments(city, state, cookie, page=1):
+    assert len(state) == 2, "Please enter state abbreviation instead of the full state."
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:124.0) Gecko/20100101 Firefox/124.0",
+        "Cookie": cookie,
+    }
     url = f"https://www.apartments.com/{city.replace(' ', '-').lower()}-{state.lower()}/{page}/"
     req = requests.get(url, headers=headers)
-    assert req.status_code == 200, f'Failed to fetch {url}'
+    assert req.status_code == 200, f"Failed to fetch {url}"
 
     return req.text
 
+
 def parse_apartments(html, apartments):
-    soup = BeautifulSoup(html, 'html.parser')
-    total_pages = int(soup.find(class_='searchResults').text.split('Page')[1][1:-1].split(' of ')[1])
+    soup = BeautifulSoup(html, "html.parser")
+    total_pages = int(
+        soup.find(class_="searchResults").text.split("Page")[1][1:-1].split(" of ")[1]
+    )
 
     # collecting apartment info if displayed via placard-header
-    header_results = soup.find_all(class_='placard-header')
+    header_results = soup.find_all(class_="placard-header")
     for result in header_results:
         apt = {}
-        apt['name'] = result.find(class_='property-title').text
-        apt['address'] = result.find(class_='property-address').text
-        apt['url'] = result.find('a', {'class': 'property-link'}).attrs['href']
+        apt["name"] = result.find(class_="property-title").text
+        apt["address"] = result.find(class_="property-address").text
+        apt["url"] = result.find("a", {"class": "property-link"}).attrs["href"]
         apartments.append(apt)
 
     # collecting any missing apartments that didn't have the placard-header
-    wrapper_results = soup.find_all(class_='property-title-wrapper')
+    wrapper_results = soup.find_all(class_="property-title-wrapper")
     for result in wrapper_results:
         apt = {}
-        apt['name'] = result.find(class_='property-title').text
-        apt['address'] = result.find(class_='property-address')['title']
-        apt['url'] = result.find('a', {'class': 'property-link'}).attrs['href']
+        apt["name"] = result.find(class_="property-title").text
+        apt["address"] = result.find(class_="property-address")["title"]
+        apt["url"] = result.find("a", {"class": "property-link"}).attrs["href"]
         apartments.append(apt)
-    
+
     return apartments, total_pages
--- srv/main.py
+++ srv/main.py
@@ -1,5 +1,5 @@
 """
-The purpose of this program is to provide a csv of all apartments in a city, state, 
+The purpose of this program is to provide a csv of all apartments in a city, state,
 and order them from least to greatest distance from a specified location.
 
 NEEDED API's:
@@ -16,60 +16,82 @@ from apartments import *
 from distance_matrix import *
 
 load_dotenv()
-DISTANCE_MATRIX_KEY = os.getenv('DISTANCE_MATRIX_KEY')
+DISTANCE_MATRIX_KEY = os.getenv("DISTANCE_MATRIX_KEY")
+COOKIE = os.getenv("COOKIE")
+
 
 def gather_inputs():
-    city_state = input("Please enter the city and state you're wanting to find apartments in (eg. Des Moines, IA): ")
-    s = city_state.split(', ')
+    city_state = input(
+        "Please enter the city and state you're wanting to find apartments in (eg. Des Moines, IA): "
+    )
+    s = city_state.split(", ")
     city = s[0]
     state = s[1]
 
-    if city == '' or state == '' or len(state) > 2 or len(state) < 2:
+    if city == "" or state == "" or len(state) > 2 or len(state) < 2:
         print("You didn't enter the city and state in the correct format. Try again.")
         gather_inputs()
 
     # Climb Iowa: East Village address -> '150 East 4th Street, Des Moines, IA 50309'
-    destination_address = input("Please enter the gym address you're wanting to calculate the apartment distance from: ")
+    destination_address = input(
+        "Please enter the gym address you're wanting to calculate the apartment distance from: "
+    )
 
     return city, state, destination_address
 
-def main(city, state, destination_address):
+
+def main(city, state, destination_address, cookie):
     apartments = []
     page_count = 1
     apartment_count = 0
 
-    print(f'Fetching apartments for {city}, {state}...')
-    html = query_apartments(city, state)
+    print(f"Fetching apartments for {city}, {state}...")
+    html = query_apartments(city, state, cookie)
     apartments, total_pages = parse_apartments(html, apartments)
-    print(f'Page 1 of {total_pages} fetched! ({len(apartments) - apartment_count} added)')
+    print(
+        f"Page 1 of {total_pages} fetched! ({len(apartments) - apartment_count} added)"
+    )
     apartment_count = len(apartments)
     page_count += 1
-    
+
     while page_count <= total_pages:
-        apartments, _ = parse_apartments(query_apartments(city, state, page_count), apartments)
-        print(f'Page {page_count} of {total_pages} fetched! ({len(apartments) - apartment_count} added)')
+        apartments, _ = parse_apartments(
+            query_apartments(city, state, cookie, page_count), apartments
+        )
+        print(
+            f"Page {page_count} of {total_pages} fetched! ({len(apartments) - apartment_count} added)"
+        )
         apartment_count = len(apartments)
         page_count += 1
 
-    print(f'All apartments fetched. (Total: {len(apartments)})')
+    print(f"All apartments fetched. (Total: {len(apartments)})")
 
-    print(f'Calculating each apartments distance from specified destination address ({destination_address})...')
-    batches = math.floor(len(apartments) / 25) # NOTE: 25 is used because that's the max amount of origin's we can do per request
+    print(
+        f"Calculating each apartments distance from specified destination address ({destination_address})..."
+    )
+    batches = math.floor(
+        len(apartments) / 25
+    )  # NOTE: 25 is used because that's the max amount of origin's we can do per request
 
-    for i in range(batches+1):
-        batch_amount = 25 # NOTE: 25 is used for the same reason as above
+    for i in range(batches + 1):
+        batch_amount = 25  # NOTE: 25 is used for the same reason as above
 
-        apartments_batch = apartments[batch_amount * i: batch_amount * (i+1)]
-        apartments_batch = distance_matrix(apartments_batch, destination_address, DISTANCE_MATRIX_KEY)
-        apartments[batch_amount * i: batch_amount * (i+1)] = apartments_batch
-        print(f'Batch {i+1} of {batches+1} calculated!')
+        apartments_batch = apartments[batch_amount * i : batch_amount * (i + 1)]
+        apartments_batch = distance_matrix(
+            apartments_batch, destination_address, DISTANCE_MATRIX_KEY
+        )
+        apartments[batch_amount * i : batch_amount * (i + 1)] = apartments_batch
+        print(f"Batch {i+1} of {batches+1} calculated!")
 
-    apartments_sorted = sorted(apartments, key=lambda obj: float(obj['distance'].split(' ')[0]))
-    with open('./result.csv', 'w', newline='') as f:
+    apartments_sorted = sorted(
+        apartments, key=lambda obj: float(obj["distance"].split(" ")[0])
+    )
+    with open("./result.csv", "w", newline="") as f:
         writer = csv.DictWriter(f, apartments_sorted[0].keys())
         writer.writeheader()
         writer.writerows(apartments_sorted)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     city, state, destination_address = gather_inputs()
-    main(city, state, destination_address)
+    main(city, state, destination_address, COOKIE)
